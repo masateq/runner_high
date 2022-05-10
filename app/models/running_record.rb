7aafe84e2c2458a077b4d19270bc00a7ab05beb6
masateq <masaya0821s@gmail.com>
2022-05-10 23:49:55 +0900

    fix: カラムのvaridate修正とエラーメッセージの定義

--- app/models/running_record.rb
+++ app/models/running_record.rb
@@ -1,10 +1,10 @@
 class RunningRecord < ApplicationRecord
   belongs_to :user
 
-  validates :running_distance, numericality: { in: 0..100 }, presence: true
-  validates :running_hour, numericality: { in: 0..8 }, presence: true
-  validates :running_minute, numericality: { in: 0..59 }, presence: true
-  validates :running_second, numericality: { in: 0..59 }, presence: true
+  validates :running_distance, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }, presence: true
+  validates :running_hour, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 8 }, presence: true
+  validates :running_minute, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 59 }, presence: true
+  validates :running_second, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 59 }, presence: true
   
   enum intensity: { E: 0, M: 1, T: 2, I: 3, R: 4 }
   
