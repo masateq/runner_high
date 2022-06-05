8f75e7baa78c756bbc9547edbfb0baeab23725b6
masateq <masaya0821s@gmail.com>
2022-06-05 18:04:08 +0900

    fix: 強度調整パラメータをランニングタイムに反映

--- app/models/training_suggestion.rb
+++ app/models/training_suggestion.rb
@@ -40,14 +40,14 @@ class TrainingSuggestion < ApplicationRecord
   # 消費カロリーの導出
   def calorie
     time = (1000 / velocity) * self.running_distance                                            # 総ランニング時間(minを小数で)
-    mets = self.user.vdot * percent / 3.5                                                       # mets導出
+    mets = self.user.vdot * (percent + self.adjust_intensity.to_f / 100) / 3.5                                                       # mets導出
     cal = (self.user.weight.present? ? mets * self.user.weight * time / 60 * 1.05 : nil).to_i   # 消費カロリー導出
     "#{cal} kcal"
   end
 
   # ランニングの速度(m/min)の導出用メソッド
   def velocity
-    vdot = self.user.vdot * percent 
+    vdot = self.user.vdot * (percent + self.adjust_intensity.to_f / 100)
     29.54 + 5.000663 * vdot - 0.007546 * vdot ** 2
   end
 
