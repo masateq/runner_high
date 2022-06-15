ee16c1b203dd4b505668327af28cf7ee74c25c9f
masateq <masaya0821s@gmail.com>
2022-06-15 22:16:08 +0900

    fix: model側でもuser情報にvalidates

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,10 +11,10 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 30 }
   validates :age, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }, allow_nil: true
   validates :weight, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 200 }, allow_nil: true
-  validates :running_distance, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }, allow_nil: true
-  validates :running_hour, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 8 }, allow_nil: true
-  validates :running_minute, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 59 }, allow_nil: true
-  validates :running_second, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 59 }, allow_nil: true
-
+  validates :running_distance, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }, presence: true
+  validates :running_hour, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 8 }, presence: true
+  validates :running_minute, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 59 }, presence: true
+  validates :running_second, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 59 }, presence: true
+ 
   enum sex: { not_specified: 0, male: 1, female: 2 }
 end
