193170ebfaed3dba3482fc6534eb5af00c9775d6
masateq <masaya0821s@gmail.com>
2022-08-22 22:57:06 +0900

    add: 利用規約 プライバシーポリシーの記述

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   resource :training_suggestions, only: %i[new create show edit update]
   
   get "tutorial" => "top_pages#tutorial"
+  
+  get "terms" => "static_pages#terms"
+  get "privacy" => "static_pages#privacy"
+
   get 'login' => 'user_sessions#new', :as => :login
   post 'login' => "user_sessions#create"
   delete 'logout' => 'user_sessions#destroy', :as => :logout
