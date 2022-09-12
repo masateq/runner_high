f477c40e88973c5088d8f7c2d411338846887811
masateq <masaya0821s@gmail.com>
2022-09-12 19:46:42 +0900

    月間走行距離の表示機能をトップページから一覧画面へと移行

--- app/controllers/running_records_controller.rb
+++ app/controllers/running_records_controller.rb
@@ -6,6 +6,15 @@ class RunningRecordsController < ApplicationController
   end
 
   def index
+    @user = User.find(current_user.id)
+    month_records = @user.running_records.order(date: :desc)
+    @month_records = []
+    month_records.each do |month_record|
+      if month_record.date.between?(Date.today - 30.day, Date.today) # 今日の日付から一週間以内の記録だけを抽出
+        @month_records << month_record
+      end
+    end
+
     @running_records = current_user.running_records.order(date: :desc)
     gon.vdot = []
     gon.date = []
