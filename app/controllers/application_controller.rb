35d40c7e0150587ffa9de62bf1f43bcfa6014e9a
masateq <masaya0821s@gmail.com>
2022-09-14 22:45:51 +0900

    add: メニューのリンク先設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :require_login
+  before_action :set_user_info
   
   private
   
+  def set_user_info
+    @user = User.find(current_user.id)
+    @training_suggestion = TrainingSuggestion.find_by(user_id: current_user.id)
+  end
+
   def not_authenticated
     redirect_to login_path
     flash[:danger] = t('defaults.login_fail')
--- app/controllers/top_pages_controller.rb
+++ app/controllers/top_pages_controller.rb
@@ -1,10 +1,7 @@
 class TopPagesController < ApplicationController
   skip_before_action :require_login, only: %i[tutorial start]
 
-  def top
-    @user = User.find(current_user.id)
-    @training_suggestion = TrainingSuggestion.find_by(user_id: current_user.id)
-  end
+
 
   def tutorial
   end
